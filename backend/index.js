4328da7c888f8e05da9275ac2f39876aeccc72a4
preetibissoyi <Preetibissoyi@gmail.com>
2023-05-07 10:29:49 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,50 @@
 const dotenv = require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyparser = require("body-parser");
+const bodyParser = require("body-parser");
 const cors = require("cors");
+const userRoute = require("./routers/userRoute");
+const productRoute = require("./routes/productRoute");
+const contactRoute = require("./routers/contactRoute");
+const errorHandler = require("./Middleware/errorMiddleware");
+const cookieParser = require("cookie-parser");
+const path = require("path");
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
-//connet to db and start server
-mongoose
-.connect(process.env.MONGO_URI)
-.then(()=>{
-    app.listen(PORT,()=>{
-       console.log(`server is running on port ${PORT}`) 
-    })
-})
-.catch((err)=>console.log(err))
+// Middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://pinvent-app.vercel.app"],
+    credentials: true,
+  })
+);
 
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+// Routes Middleware
+app.use("/api/users", userRoute);
+//app.use("/api/products", productRoute);
+//app.use("/api/contactus", contactRoute);
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
+
+// Error Middleware
+app.use(errorHandler);
+// Connect to DB and start server
+const PORT = process.env.PORT || 5000;
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server Running on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
---
+++ backend/routers/userRoute.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+const {
+    registerUser,
+    loginUser,
+    logout,
+    getUser,
+    loginStatus,
+    updateUser,
+    changePassword,
+    forgotPassword,
+    resetPassword,
+  } = require("../controllers/userController");
+const protect = require("../Middleware/authMiddleware");
+
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.post("/logout", logout);
+router.get("/getuser",protect,getUser);
+router.get("/loggedin",loginStatus);
+router.patch("/updateuser", protect, updateUser);
+router.patch("/changepassword", protect, changePassword);
+router.post("/forgotpassword", forgotPassword);
+router.put("/resetpassword/:resetToken", resetPassword);
+
+
+module.exports =router
